8fbe6e46d2375eeef3febe6e98b07fe62573add9
Mohamed Abidar <mohamed.abidaar@gmail.com>
2022-12-05 13:35:40 +0100

    Add repair wordpress database

---
+++ repair-wordpress-database.php
@@ -0,0 +1,7 @@
+<?php
+
+// Add this line in wp-config.php
+// And you can see the settings by visiting this page: http://www.yoursite.com/wp-admin/maint/repair.php
+// Once you are done repairing and optimizing your database, make sure to remove this code from your wp-config.php.
+
+define('WP_ALLOW_REPAIR', true);
